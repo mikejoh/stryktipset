32702d74013c2a19ab78d0a90d61cd4918bfb0fe
mikejoh <mik.json@gmail.com>
2018-11-01 21:11:04 +0100

    Added simple example on how to use the API in a CLI command

--- cmd/st/main.go
+++ cmd/st/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "flag"
-import st "github.com/mikejoh/stryktipset"
+import (
+	"flag"
+	"fmt"
+
+	st "github.com/mikejoh/stryktipset"
+)
 
 func main() {
-	sekPtr := flag.Int("sek", 0, "Amount of SEK to bet.")
+	sekPtr := flag.Int("sek", 2, "Amount of SEK to bet.")
 	flag.Parse()
 
-	st.ConvertSekToBet()
+	full, half := st.ConvertSekToBet(*sekPtr)
+	fmt.Printf("Full: %d\nHalf: %d\n", full, half)
 }
