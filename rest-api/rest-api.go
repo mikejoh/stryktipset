0201acb55a1d8a83f87f9cb023c512ec8e11acca
mikejoh <mik.json@gmail.com>
2018-11-04 21:07:12 +0100

    Renamed main.go

--- rest-api/rest-api.go
+++ rest-api/rest-api.go
@@ -11,12 +11,15 @@ import (
 	"github.com/mikejoh/stryktipset"
 )
 
+// Bet type
 type Bet struct {
 	Sek  int `json:"sek"`
 	Full int `json:"full"`
 	Half int `json:"half"`
 }
 
+// Convert will convert a given amount of SEK (Swedish Crowns) to the amount of full and half covers you can bet
+// Returns an JSON encoded Bet type
 func Convert(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
@@ -32,6 +35,7 @@ func Convert(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(b)
 }
 
+// RandomizedBet returns a JSON encoded array of 13 randomized bets
 func RandomizedBet(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
