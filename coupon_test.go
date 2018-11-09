193e2923088657511f4f9d78614e99d301ae733e
mikejoh <mik.json@gmail.com>
2018-11-09 22:11:50 +0100

    Chnaged variable names

--- coupon_test.go
+++ coupon_test.go
@@ -5,27 +5,27 @@ import "testing"
 func TestCreateCoupon(t *testing.T) {
 	c := NewCoupon()
 
-	expectedFull := 1
-	expectedHalf := 6
-	expectedSingles := 5
+	wantFull := 1
+	wantHalf := 6
+	wantSingles := 5
 
-	returnedFull := 0
-	returnedHalf := 0
-	returnedSingles := 0
+	gotFull := 0
+	gotHalf := 0
+	gotSingles := 0
 
-	c.Create(expectedFull, expectedHalf)
+	c.Create(wantFull, wantHalf)
 
 	for _, b := range c.Bets {
 		if len(b.String()) == 3 {
-			returnedFull++
+			gotFull++
 		} else if len(b.String()) == 2 {
-			returnedHalf++
+			gotHalf++
 		} else if len(b.String()) == 1 {
-			returnedSingles++
+			gotSingles++
 		}
 	}
 
-	if returnedFull != expectedFull && returnedHalf != expectedHalf && returnedSingles != expectedSingles {
-		t.Errorf("CreateCoupon(\"%d, %d\") returned %d full, %d half and %d singles. Expected %d, %d and %d", expectedFull, expectedHalf, returnedFull, returnedHalf, returnedSingles, expectedFull, expectedHalf, expectedSingles)
+	if gotFull != wantFull && gotHalf != wantHalf && gotSingles != wantSingles {
+		t.Errorf("CreateCoupon() got = %d full, %d half and %d singles. want %d, %d and %d", wantFull, wantHalf, gotFull, gotHalf, gotSingles, wantFull, wantHalf, wantSingles)
 	}
 }
