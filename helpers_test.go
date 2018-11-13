80b36b9e27f0ed4041c1f28b96681b362c3204a1
mikejoh <mik.json@gmail.com>
2018-11-13 11:44:37 +0100

    Rewrote and added test

--- helpers_test.go
+++ helpers_test.go
@@ -2,21 +2,38 @@ package stryktipset
 
 import "testing"
 
-type TestBet struct {
-	sek          int
-	expectedFull int
-	expectedHalf int
-}
-
-var testBets = []TestBet{
-	{2, 0, 1}, {4, 0, 2}, {8, 0, 3}, {16, 0, 4}, {48, 1, 4}, {96, 1, 5}, {144, 2, 4}, {432, 3, 4},
+var bettests = []struct {
+	sek             int
+	expectedFull    int
+	expectedHalf    int
+	expectedSingles int
+}{
+	{2, 0, 1, 12},
+	{4, 0, 2, 11},
+	{8, 0, 3, 10},
+	{16, 0, 4, 9},
+	{48, 1, 4, 8},
+	{96, 1, 5, 7},
+	{192, 1, 6, 6},
+	{144, 2, 4, 7},
+	{432, 3, 4, 6},
 }
 
 func TestConvertSekToBet(t *testing.T) {
-	for _, tt := range testBets {
-		full, half := ConvertSekToBet(tt.sek)
-		if full != tt.expectedFull && half != tt.expectedHalf {
-			t.Errorf("ConvertToCovers(%d) should return %d %d but we got %d %d", tt.sek, tt.expectedFull, tt.expectedHalf, full, half)
+	for _, tt := range bettests {
+		gotFull, gotHalf := ConvertSekToBet(tt.sek)
+		if gotFull != tt.expectedFull && gotHalf != tt.expectedHalf {
+			t.Errorf("ConvertToCovers() got = %d %d, want = %d %d", tt.expectedFull, tt.expectedHalf, gotFull, gotHalf)
+		}
+	}
+}
+
+func TestConvertSekToBetNumberOfSingles(t *testing.T) {
+	for _, tt := range bettests {
+		gotFull, gotHalf := ConvertSekToBet(tt.sek)
+		gotSingles := 13 - (gotFull + gotHalf)
+		if gotSingles != tt.expectedSingles {
+			t.Errorf("ConvertToCovers(%d) got = %d singles, want = %d singles", tt.sek, tt.expectedSingles, gotSingles)
 		}
 	}
 }
