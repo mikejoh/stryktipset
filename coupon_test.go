fbf6dd21487fda7b8e8df42b83488c0955d77a4a
mikejoh <mik.json@gmail.com>
2018-11-13 11:44:22 +0100

    Rewrote test

--- coupon_test.go
+++ coupon_test.go
@@ -2,30 +2,38 @@ package stryktipset
 
 import "testing"
 
+var coupontests = []struct {
+	wantFull    int
+	wantHalf    int
+	wantSingles int
+}{
+	{0, 6, 7},
+	{1, 5, 6},
+	{1, 6, 5},
+}
+
 func TestCreateCoupon(t *testing.T) {
-	c := NewCoupon()
+	for _, tt := range coupontests {
+		c := NewCoupon()
 
-	wantFull := 1
-	wantHalf := 6
-	wantSingles := 5
+		gotFull := 0
+		gotHalf := 0
+		gotSingles := 0
 
-	gotFull := 0
-	gotHalf := 0
-	gotSingles := 0
+		c.Create(tt.wantFull, tt.wantHalf)
 
-	c.Create(wantFull, wantHalf)
-
-	for _, b := range c.Bets {
-		if len(b.String()) == 3 {
-			gotFull++
-		} else if len(b.String()) == 2 {
-			gotHalf++
-		} else if len(b.String()) == 1 {
-			gotSingles++
+		for _, b := range c.Bets {
+			if len(b.String()) == 3 {
+				gotFull++
+			} else if len(b.String()) == 2 {
+				gotHalf++
+			} else if len(b.String()) == 1 {
+				gotSingles++
+			}
 		}
-	}
 
-	if gotFull != wantFull && gotHalf != wantHalf && gotSingles != wantSingles {
-		t.Errorf("CreateCoupon() got = %d full, %d half and %d singles. want %d, %d and %d", gotFull, gotHalf, gotSingles, wantFull, wantHalf, wantSingles)
+		if gotFull != tt.wantFull && gotHalf != tt.wantHalf && gotSingles != tt.wantSingles {
+			t.Errorf("CreateCoupon() got = %d full, %d half and %d singles. want %d, %d and %d", gotFull, gotHalf, gotSingles, tt.wantFull, tt.wantHalf, tt.wantSingles)
+		}
 	}
 }
